bbeff7f862e0d57ff2259da876f0b279190aef11
Melissa Stock <melissastock44@gmail.com>
2020-01-26 16:25:22 -0800

    initialize

---
+++ MS_Sprint1_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MS_Sprint1_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
